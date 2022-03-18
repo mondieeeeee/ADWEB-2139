4e50c3e66d33177d7139c40239e8b3b244f693d7
Almond Rosos <83632147+mondieeeeee@users.noreply.github.com>
2022-03-18 15:40:08 +0800

    Date Pipes

---
+++ Midterm Discussion Activity 2/test/test.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable, Observer } from 'rxjs'; // new import
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent implements OnInit {
+
+  //date pipes
+  presentDate = new Date();
+
+  //asyncpipe
+  time = new Observable<string>((observer: Observer<string>) => {
+    setInterval(() => observer.next(new Date().toString()), 1000);
+  });
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
